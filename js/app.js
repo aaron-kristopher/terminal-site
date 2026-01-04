7a2d36b5a4a410196a5df690d991e46ae31d7c30
aaron-kristopher <aaron.lim.cstr@gmail.com>
2026-01-04 22:02:39 +0800

    feat: Add history of commands and allow for traversal in command history

--- js/app.js
+++ js/app.js
@@ -4,17 +4,54 @@ import { renderer } from "./renderer.js";
 
 const input = document.getElementById("command");
 
+let commandHistory = [];
+let commandHistoryPointer;
 
 input.addEventListener("keydown", (e) => {
+
+	// Check pointer value to ensure it doesn't go out of bounds
+	if (e.key === "ArrowUp" && commandHistoryPointer > 0) {
+		e.preventDefault();
+		input.value = commandHistory[--commandHistoryPointer];
+	}
+
+	else if (e.key == "ArrowDown") {
+		e.preventDefault();
+		if (commandHistoryPointer < commandHistory.length - 1) {
+			// We move forward in history
+			input.value = commandHistory[++commandHistoryPointer];
+		}
+		else {
+			// We are at the bottom, so we clear the input (new prompt)
+			input.value = ""
+		}
+	}
+
+	// Continue gathering input until user clicks enter
 	if (e.key !== "Enter") return;
 
 	const userInput = input.value.trim().toLowerCase();
 
-	if (!commands[userInput]) {
+	// Handle empty string input like regular terminal
+	// Do not show error. Just print empty output and show new prompt
+	// Do not add to command history
+	if (!userInput)
+		renderer.render({ type: "text", content: "", command: "" });
+
+	else if (!commands[userInput])
 		renderer.renderError(userInput);
-	} else {
+
+	else {
 		const output = commands[userInput].execute(portfolioData);
 		renderer.render(output);
+	}
+
+	// Add input to history of commands and
+	// increment the pointer
+	if (userInput) {
+		commandHistory.push(userInput);
+		commandHistoryPointer = commandHistory.length;
+		console.log("Commands: ", commandHistory);
 	}
 
 	input.value = "";
